8f84573cd74234cd86046dc27b57af1c07407f40
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-24 22:02:05 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
+        <ol> <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          <li>Save and see your changes instantly.</li> </ol>
 
         <div className={styles.ctas}>
           <a
@@ -26,8 +24,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            <Image
+          > <Image
               className={styles.logo}
               src="/vercel.svg"
               alt="Vercel logomark"
@@ -42,8 +39,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
-          </a>
+            Read our docs </a>
         </div>
       </main>
       <footer className={styles.footer}>
