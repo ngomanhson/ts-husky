5934d614a285ae0b6dd7540d17556fc844490d75
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-24 22:04:33 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
         />
         <ol> <li>
             Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li> </ol>
+          </li><li>Save and see your changes instantly.</li> </ol>
 
         <div className={styles.ctas}>
           <a
