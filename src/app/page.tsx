a6f526e42f8360ffdbfa25a9f44a1741a2c2d985
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-24 22:09:40 +0700

    test 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+        <ol><li>Get started by editing <code>src/app/page.tsx</code>.
           </li><li>Save and see your changes instantly.</li> </ol>
 
         <div className={styles.ctas}>
