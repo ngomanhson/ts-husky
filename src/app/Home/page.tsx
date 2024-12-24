e18f41aadcdd5df01262c1a75b9abe12daabccb1
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-24 22:19:13 +0700

    create ui dashboard

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>asdasd</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+    );
+}
