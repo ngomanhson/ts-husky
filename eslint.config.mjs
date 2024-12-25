d36c3e4fad5b8ac92dcf86388f7475ce49a0bc8d
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-25 22:35:42 +0700

    sda

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,19 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//     ...compat.extends('next/core-web-vitals', 'next/typescript'),
+// ];
+
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'no-console': 'warn',
+        },
+    },
 ];
+
 
 export default eslintConfig;
--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,13 +1,15 @@
 export default function Dashboard() {
+const dashboard = 'Dashboard';
+
     return (
-        <div>
-            <h1>asdasd</h1>
+            <div>
+                     <h1>asdasd</h1>
             <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
+                            <li>1</li>
+                             <li>2</li>
+                            <li>3</li>
                 <li>4</li>
-                <li>5</li>
+                        <li>5</li>
             </ul>
         </div>
     );
